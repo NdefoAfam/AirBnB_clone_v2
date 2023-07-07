c2bdb903123c25356d287f8301d2a1f40f5f8757
Ndefo Afam <ndefoafam@gmail.com>
2023-07-07 16:03:52 +0100

    Update 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
+""" Fabric script that generates a .tgz archive
+  from the contents of the web_static folder """
+
+from datetime import datetime
 from fabric.api import local
-from time import strftime
-from datetime import date
 
 
 def do_pack():
-    """ A script that generates archive the contents of web_static folder"""
+    """Generates a .tgz archive from the contents
+    of the web_static folder of this repository.
+    """
 
-    filename = strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
+    d = datetime.now()
+    now = d.strftime('%Y%m%d%H%M%S')
 
-        return "versions/web_static_{}.tgz".format(filename)
+    local("mkdir -p versions")
+    archive_path = "versions/web_static_{}.tgz".format(now)
+    result = local("tar -czvf {} web_static".format(archive_path))
 
-    except Exception as e:
+    if result.succeeded:
+        return archive_path
+    else:
         return None
